acc2d63d042e0771f126087bc7e68c08cd4c4be3
2018DHY <dhy3976048@icloud.com>
2025-12-11 17:00:36 +0800

    update

--- example/net_api.cpp
+++ example/net_api.cpp
@@ -2,6 +2,8 @@
 
 #include <arpa/inet.h>
 
+#define pft   printf("==========%s==========\n",__func__)
+
 /**
  * 字节序转换函数
  * 头文件 #include <arpa/inet.h>
@@ -17,6 +19,7 @@
 
 void byt_seq_test()
 {
+    pft;
     int hostl = 0x01020304;
     int netl = 0;
     short hosts = 0x0506;
@@ -30,6 +33,7 @@ void byt_seq_test()
 
 /**
  *点分十进制转化函数(用于将IP字符串转化为字节序列)
+ * 头文件 #include <arpa/inet.h>
  * int inet_pton(int af, const char *restrict src, void *restrict dst); 点分十进制字符串转网络字节序列
  * af 表示地址族类型AF_INET:IPV4 AF_INET6:IPV6
  * src 字符串地址
@@ -37,11 +41,38 @@ void byt_seq_test()
  * 返回值 1为成功
  * 
  * const char *inet_ntop(int af, const void *restrict src, char dst[restrict .size], socklen_t size);
+ * af 表示地址族类型AF_INET:IPV4 AF_INET6:IPV6
+ * src 源
+ * dst 字符串指针
+ * size 字符串缓冲区大小
  * 
  */
+
+ /**
+  * socket 获得一个套接字 
+  * 头文件 #include <sys/socket.h>
+  * 
+  * int socket(int domain, int type, int protocol);
+  * 
+  */
+
+ void str_to_int_test(){
+    pft;
+    uint32_t ip_int=0;
+    inet_pton(AF_INET,"192.168.1.1",&ip_int);
+    u_int8_t* ptr=(u_int8_t*)&ip_int;
+    printf("str to int is %d %d %d %d\n",ptr[0],ptr[1],ptr[2],ptr[3]);
+
+    uint32_t ip_int_r=0x01020304;
+    char buf[INET_ADDRSTRLEN]="";
+    inet_ntop(AF_INET,&ip_int_r,buf,sizeof(buf));
+    printf("int to str is %s\n",buf);
+ }
 
 int main(int argc, char *argv[])
 {
     byt_seq_test();
+    str_to_int_test();
+
     return 0;
 }
